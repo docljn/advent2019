62322db87c60c7ab33e8603f3b04a6fef3941c51
DocLJN <28825519+docljn@users.noreply.github.com>
2019-12-01 23:17:52 +0000

    recursive example

--- lib/fuel_counter_upper.rb
+++ lib/fuel_counter_upper.rb
@@ -11,19 +11,45 @@ class FuelCounterUpper
     total_fuel(array)
   end
 
-  def calculate(module_mass)
-    (module_mass.to_i / 3).to_i - 2
-  end
-
   def total_fuel(module_masses)
     total = 0
     module_masses.each do |mass|
-      total += calculate(mass)
+      total += calculate_recursive(mass)
     end
     total
   end
 
   def read_file(file_path)
     CSV.read(file_path)
+  end
+
+  private
+
+  # def calculate(module_mass)
+  #   total_fuel = 0
+  #   fuel = fuel_for_mass(module_mass)
+  #   while fuel.positive?
+  #     total_fuel += fuel
+  #     fuel = fuel_for_mass(fuel)
+  #   end
+  #   total_fuel
+  # end
+
+  def fuel_for_mass(mass)
+    (mass.to_i / 3).to_i - 2
+  end
+
+  def calculate_recursive(module_mass)
+    total_fuel = 0
+    added_fuel = fuel_for_mass(module_mass)
+    calculate_recursion(module_mass, total_fuel, added_fuel)
+  end
+
+  def calculate_recursion(module_mass, total_fuel, added_fuel)
+    return total_fuel if added_fuel <= 0
+
+    total_fuel += added_fuel
+    added_fuel = fuel_for_mass(added_fuel)
+    calculate_recursion(module_mass, total_fuel, added_fuel)
   end
 end
--- spec/fuel_counter_upper_spec.rb
+++ spec/fuel_counter_upper_spec.rb
@@ -1,30 +1,31 @@
 require './lib/fuel_counter_upper'
 
 RSpec.describe FuelCounterUpper do
-
   let(:counter) { FuelCounterUpper.new }
 
-  it "returns 2 when given 12" do
-    expect(counter.calculate(12)).to eq 2
+  it 'returns 2 when given [12]' do
+    expect(counter.total_fuel([12])).to eq 2
   end
 
-  it "returns 2 when given 14" do
-    expect(counter.calculate(14)).to eq 2
+  it 'returns 2 when given 14' do
+    expect(counter.total_fuel([14])).to eq 2
   end
 
-  it "returns 654 when given 1969" do
-    expect(counter.calculate(1969)).to eq 654
-  end
-
-  it "returns 33583 when given 100756" do
-    expect(counter.calculate(100_756)).to eq 33_583
-  end
-
-  it "returns 4 when given 12 and 14" do
+  it 'returns 4 when given [12, 14]' do
     expect(counter.total_fuel([12, 14])).to eq 4
   end
 
-  it "returns an answer when given a file" do
-    expect(counter.from_file('./spec/module_masses.csv')).to be(0)
+  context 'fuel has a mass' do
+    it 'returns 966 when given 1969' do
+      expect(counter.total_fuel([1969])).to eq 966
+    end
+
+    it 'returns 50,346 when given 100756' do
+      expect(counter.total_fuel([100_756])).to eq 50_346
+    end
+  end
+
+  it 'returns an answer when given a file' do
+    expect(counter.from_file('./spec/module_masses.csv')).to be_truthy
   end
 end
